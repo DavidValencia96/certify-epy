e4b3fedc6d95bb23ab19316b525558523a083d64
David Valencia <63324171+DavidValencia96@users.noreply.github.com>
2023-04-05 18:36:22 -0500

    Actualizar index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <link rel="icon" href="./img/favicon.png">
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/loader.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link href="https://cdnjsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdnjsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://checkout.epayco.co/checkout.js"></script>
@@ -122,11 +122,11 @@
     </div>
     
 </body>
-<script src="/js/data.js"></script>
-<script src="/js/function.js"></script>
-<!-- <script src="/js/check.js"></script> -->
-<script src="/js/payment.js"></script>
-<script src="/js/loader.js"></script>
+<script src="./js/data.js"></script>
+<script src="./js/function.js"></script>
+<!-- <script src="./js/check.js"></script> -->
+<script src="./js/payment.js"></script>
+<script src="./js/loader.js"></script>
 <script src="http://code.jquery.com/jquery.js"></script>
 </html>
 
